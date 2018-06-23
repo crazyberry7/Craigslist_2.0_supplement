f43cdbf27ad99a6a7a5941f4b8eb844d9143d365
Kevin <crazyberry7@gmail.com>
2018-06-22 22:19:55 -0700

    Added Bootstrap for login and registration

--- users/models.py
+++ users/models.py
@@ -38,6 +38,7 @@ class customuser(AbstractUser):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     email = models.CharField(max_length = 50)
+    username = models.CharField(max_length = 30)
 	
     def __str__(self):
-      	return self.first_name + self.last_name
+      	return self.username
